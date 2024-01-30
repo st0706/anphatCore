62780c87f4133fe33aebccacf50c83ef4f970900
Nguyen Son Tung <1stgemini123@gmail.com>
2024-01-31 00:21:37 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "anphat-core.vercel-mu.app", "unsplash.com", "utfs.io"]
+    domains: ["localhost", "anphat-core.vercel.app", "pacs.store", "dev.pacs.store", "unsplash.com", "utfs.io"]
   },
   eslint: {
     ignoreDuringBuilds: true
